797495568f5cfc55bfbc5403d5b8ca846efd6fff
Viny <viniciusfernandes1357@gmail.com>
2023-12-01 16:16:14 -0300

    - primeiro commit Alysson

---
+++ dicionario.py
@@ -0,0 +1,27 @@
+# aula4 Python Turma 21 - 29/11/2023
+# programa para retornar uma lista de dados a partir de um dicionário
+
+# Apresentação
+print('\n\t\t\t -- Cadastro de Produtos --\n')
+
+# Entrada de dados
+nomeProduto = input("Digite o nome do produto: ")
+quantidadeProduto = int(input("Digite a quantidade do produto: "))
+valorProduto = float(input("Digite o preço unitário do produto: "))
+disponivelProduto = input("Produto se encontra disponível? (S/N): ")
+
+# Verificar a disponibilidade
+if disponivelProduto.upper() == 'S':
+    disponibilidade = True
+else:
+    disponibilidade = False
+
+# Dicionário com os valores inseridos pelo usuário
+dados_produto = {'nome': nomeProduto, 'quantidade': quantidadeProduto, 'preço': valorProduto, "disponibilidade": disponibilidade}
+
+# Saída
+print('\nCadastrado efetuado com sucesso',
+      '\nProduto: ', (dados_produto['nome']),
+      '\nQuantidade: ', (dados_produto['quantidade']),
+      '\nPreço: R$ ', (dados_produto['preço']),
+      '\nDisponível: ', (dados_produto['disponibilidade']))
